db528c0beb5d6f694e0b9f59652023513641a368
Gauthier SAMBA SEALE <gssbartender@gmail.com>
2023-03-09 19:23:26 +0100

    first

---
+++ src/api.js
@@ -0,0 +1,45 @@
+export const fetchResponse = async (chat) => {
+    try {
+
+        // const response = await fetch('https://localhost:3080/', {
+        // const response = await fetch('http://localhost:3080/', {
+        const response = await fetch('http://localhost:3080/', {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                message: chat.map((message) => message.message).join(" \n ")
+            })
+        })
+
+        const data = await response.json()
+
+        return data
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// export const Response = async (chat) => {
+//     try {
+//       const response = await fetch('http://localhost:3080/', {
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify({
+//           message: chat.map((message) => message.message).join(' \n ')
+//         })
+//       })
+  
+//       const text = await response.text()
+//       const data = JSON.parse(text)
+  
+//       return data
+//     } catch (error) {
+//       console.log(error)
+//     }
+//   }
+  
